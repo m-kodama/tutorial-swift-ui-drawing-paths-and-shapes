d473c2a673a14c4369f2ebf40b570e51a8c4196b
m-kodama <m-kodama@rhizo-me.com>
2019-09-30 18:00:05 +0900

    complete section 1
    - Badge.swiftを作っただけ

---
+++ Landmarks/Landmarks/Badge.swift
@@ -0,0 +1,21 @@
+//
+//  Badge.swift
+//  Landmarks
+//
+//  Created by m-kodama on 2019/09/30.
+//  Copyright © 2019 Apple. All rights reserved.
+//
+
+import SwiftUI
+
+struct Badge: View {
+    var body: some View {
+        Text("Badge")
+    }
+}
+
+struct Badge_Previews: PreviewProvider {
+    static var previews: some View {
+        Badge()
+    }
+}
